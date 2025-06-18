e9273bd3b9fed5d5865af01e226aa46afde5f1c1
christian <ckimescamilla@gmail.com>
2025-06-18 17:30:02 +0800

    adde Post CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -8,3 +9,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::resource('posts', PostController::class); // This will create all the routes for the PostController
